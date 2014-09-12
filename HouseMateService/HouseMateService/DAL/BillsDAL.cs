713a9d99b8bb3c38fb37a125eaf023073c7f4a4d
RomeshPanditha <c3131416@uon.edu.au>
2014-09-13 01:06:29 +1000

    started bills

--- HouseMateService/HouseMateService/DAL/BillsDAL.cs
+++ HouseMateService/HouseMateService/DAL/BillsDAL.cs
@@ -14,5 +14,43 @@ namespace HouseMateService
         {
             
         }
+
+        public List<Bill> getBills(int houseID)
+        {
+            List<Bill> billList = new List<Bill>();
+            using(var context = new houseMateEntities())
+            {
+                List<house_bill> billL = new List<house_bill>();
+                billL.AddRange(from house_bill in context.house_bill
+                               where house_bill.FK_houseID == houseID
+                               select house_bill);
+
+                int tNum = getNumTenants(houseID);
+
+                foreach (house_bill h in billL)
+                {
+                    billList.Add(new Bill(h.PK_houseBillID, h.amount, h.billType, h.dueDate, tNum));
+                    
+                }
+            }
+            return billList;
+        }
+
+        private int getNumTenants(int houseID)
+        {
+            int numTenants = 0;
+            using(var context = new houseMateEntities())
+            {
+                List<tenant> tenantList = new List<tenant>();
+                tenantList.AddRange(from tenant in context.tenants
+                                    where tenant.FK_houseID == houseID
+                                    select tenant);
+                foreach(tenant t in tenantList)
+                {
+                    numTenants++;
+                }
+            }
+            return numTenants;
+        }
     }
 }
---
+++ HouseMateService/HouseMateService/Models/Bill.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace HouseMateService.Models
+{
+    [DataContract]
+    public class Bill
+    {
+     
+        private int billID_;
+        private double totalAmount_;
+        private string category_;
+        private DateTime dueDate_;
+        private string[] tenantNames;
+        
+
+        public Bill(int billID, double totalAmount, string category, DateTime dueDate, int numTenants)
+        {
+            int billID_ = billID;
+            double totalAmount_ = totalAmount;
+            string category_ = category;
+            DateTime dueDate_ = dueDate;
+            tenantNames = new string[numTenants];
+        }
+
+
+
+        [DataMember]
+        public int billID { get { return billID_; } set { billID_ = value; } }
+        [DataMember]
+        public double totalAmount { get { return totalAmount_; } set { totalAmount_ = value; } }
+        [DataMember]
+        public string category { get { return category_; } set { category_ = value; } }
+        [DataMember]
+        public DateTime dueDate { get { return dueDate_; } set { dueDate_ = value; } }
+
+    }
+    
+}
