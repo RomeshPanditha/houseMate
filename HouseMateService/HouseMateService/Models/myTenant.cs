f40f20903f3bdac02bf849303418614f52258feb
RomeshPanditha <c3131416@uon.edu.au>
2014-10-23 23:18:44 +1100

    added tenantstuff

---
+++ HouseMateService/HouseMateService/Models/myTenant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace HouseMateService.Models
+{
+    [DataContract]
+    public class myTenant
+    {
+        private int tenID_;
+        private string tenName_;
+
+        public myTenant(int tenID, string tenName)
+        {
+            tenID_ = tenID;
+            tenName_ = tenName;
+        }
+
+        [DataMember]
+        public int tenID { get { return tenID_; } set { tenID_ = value; } }
+        [DataMember]
+        public string tenName { get { return tenName_; } set { tenName_ = value; } }
+    }
+}
